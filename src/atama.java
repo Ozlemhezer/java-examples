8c746a6d63c869e43190d03418f3377206f3c5fe
Ozlemhezer <ozlemhezer65@gmail.com>
2026-05-23 16:44:42 +0300

    string blokları

--- src/atama.java
+++ src/atama.java
@@ -27,8 +27,24 @@ public class atama {
 //        System.out.println("abc".startsWith("A"));
 //        System.out.println("abc".contains("b"));
 //        System.out.println("abc".contains("B"));
-        System.out.println("abca".replace('a','A')); // char alıyor.
-        System.out.println("abca".replace("a","A")); // charSequence alıyor.
+//        System.out.println("abca".replace('a','A')); // char alıyor.
+//        System.out.println("abca".replace("a","A")); // charSequence alıyor
+        var block = """
+                Özlem HEZER
+                """;
+        System.out.println(block);
+        var block2 = """
+                ÖZlem HEZER""";
+        System.out.println(block2);
+        var block3 = """
+                    Elma
+                Armut
+                """;
+        System.out.println(block3);
+        var block4 = """
+                    deneme
+                """;
+        System.out.println(block4);
 
 
     }
