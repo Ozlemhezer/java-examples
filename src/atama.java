1be89bab5cdcf391dbaa7ec196b4c6852617216e
Ozlemhezer <ozlemhezer65@gmail.com>
2026-05-23 16:17:38 +0300

    Adjusting case, checking for equality, searching for Substrings başlıkları çalışıldı

--- src/atama.java
+++ src/atama.java
@@ -6,13 +6,28 @@ public class atama {
 //        String four = "4";
 //        var type = 1 + 2 + three + four;
 //        System.out.println((Object) type.getClass().getName());
-        var  uzunlugu_merak_edilen_nesne = new String("Nermin");
-        System.out.println(uzunlugu_merak_edilen_nesne.length()); // gerçek uzunluğunu yazdırır.
-        System.out.println("3. index'teki karakter ne ? = "+ uzunlugu_merak_edilen_nesne.charAt(3));
-        var a = "Beypazarı";
-        System.out.println(a.indexOf("za",2)); //Eğer eşleşen değer bulamasaydı -1 dönerdi.
-        System.out.println(a.substring(0,2));
-        System.out.println(a.substring(a.indexOf("za"),8)); //zar olur çıktı.
+//        var  uzunlugu_merak_edilen_nesne = new String("Nermin");
+//        System.out.println(uzunlugu_merak_edilen_nesne.length()); // gerçek uzunluğunu yazdırır.
+//        System.out.println("3. index'teki karakter ne ? = "+ uzunlugu_merak_edilen_nesne.charAt(3));
+//        var a = "Beypazarı";
+//        System.out.println(a.indexOf("za",2)); //Eğer eşleşen değer bulamasaydı -1 dönerdi.
+//        System.out.println(a.substring(0,2));
+//        System.out.println(a.substring(a.indexOf("za"),8)); //zar olur çıktı.
+//        var name = "animals";
+//        System.out.println(name.toUpperCase());
+//        System.out.println(name.toLowerCase());
+//        System.out.println(name); //String'ler immutable özelliktedir. Değişmezler.
+//        System.out.println("abc".equals("ABC"));
+//        System.out.println("ABC".equals("ABC"));
+//        System.out.println("abc".equalsIgnoreCase("ABC")); // case'i önemsemez.
+//        System.out.println("abc".endsWith("c"));
+//        System.out.println("abc".endsWith("C")); //false
+//        System.out.println("abc".startsWith("a"));
+//        System.out.println("abc".startsWith("b"));
+//        System.out.println("abc".startsWith("A"));
+//        System.out.println("abc".contains("b"));
+//        System.out.println("abc".contains("B"));
+
 
     }
 }
